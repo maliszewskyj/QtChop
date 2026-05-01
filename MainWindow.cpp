496e61f407ad36df145a8c4f0fa1cdee4c3c1dcf
Nick Maliszewskyj <ncnr@dcschopper.ncnr.nist.gov>
2026-05-01 16:48:00 -0400

    Checkpoint 5/1/2026 - not yet a program

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,66 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::SetupSerial()
+{
+    // Should get this from configuration
+    m_ChopperPortName = "/dev/ttyChopper";
+    m_BaudRate = QSerialPort::Baud57600;
+    m_DataBits = QSerialPort::Data8;
+    m_Parity   = QSerialPort::NoParity;
+    m_StopBits = QSerialPort::OneStop;
+    m_FlowControl = QSerialPort::NoFlowControl;
+
+    m_IncomingChopper = "";
+    m_IncomingHost = "";
+
+    qDebug() << "Comms opening serial port " + m_ChopperPortName;
+    m_ChopperPort = new QSerialPort();
+    m_ChopperPort->setPortName(m_ChopperPortName);
+    m_ChopperPort->setBaudRate(m_BaudRate);
+    m_ChopperPort->setDataBits(m_DataBits);
+    m_ChopperPort->setParity(m_Parity);
+    m_ChopperPort->setStopBits(m_StopBits);
+    m_ChopperPort->setFlowControl(m_FlowControl);
+
+    if (!m_ChopperPort->open(QIODevice::ReadWrite)) {
+        qDebug() << "Cannot open serial port: " + m_ChopperPortName + "\n";
+    } else {
+        qDebug() << "Opened serial port " + m_ChopperPortName + "\n";
+    }
+    connect(m_ChopperPort,SIGNAL(readyRead()), this, SLOT(onReadChopper()));
+}
+
+// From cute "GetBlock"
+void MainWindow::RequestBlock(int index)
+{
+    if (index < 0 || index > NoOfChoppers) return; //
+    m_OutgoingChopper = "#" + QByteArray::number(index) + "0000000$";
+    m_ChopperPort->write(m_OutgoingChopper);
+}
+
+void MainWindow::Refresh()
+{
+
+}
+
+// From cute "GetDataString"
+void MainWindow::onReadChopper()
+{
+    QByteArray input;
+
+    input = m_ChopperPort->readAll();
+    m_IncomingChopper.append(input);
+    if (m_IncomingChopper.contains('$')) {
+        QStringList list  = m_IncomingChopper.split('$');
+        if (list[0].length()==0) {
+            m_IncomingChopper = "";
+        } else {
+            // Process the whole block
+        }
+    }
+}
+void MainWindow::onErrorChopper(QSerialPort::SerialPortError error)
+{
+
+}
--- MainWindow.h
+++ MainWindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QRegularExpression>
+#include <QDebug>
+#include "Chop_Def.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +18,30 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    QString m_ChopperPortName;
+    QString m_HostPortName;
+    QSerialPort * m_ChopperPort;
+    QSerialPort * m_HostPort;
+
+    QSerialPort::BaudRate m_BaudRate;
+    QSerialPort::DataBits m_DataBits;
+    QSerialPort::Parity   m_Parity;
+    QSerialPort::StopBits m_StopBits;
+    QSerialPort::FlowControl m_FlowControl;
+
+    QString m_IncomingChopper;
+    QByteArray m_OutgoingChopper;
+    QString m_IncomingHost;
+    QByteArray m_OutgoingHost;
+
+    void SetupSerial();
+
+    void RequestBlock(int index);
+public slots:
+    void Refresh();
+    void onReadChopper();
+    void onErrorChopper(QSerialPort::SerialPortError error);
 
 private:
     Ui::MainWindow *ui;
